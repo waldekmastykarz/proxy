85cfe4ca840af3df84753959fccedab4f10ba702
waldekmastykarz <waldek@mastykarz.nl>
2022-03-01 17:03:00 +0100

    Graph chaos

--- proxy.js
+++ proxy.js
@@ -428,6 +428,31 @@ function onconnect(req, socket, head) {
 		var opts = { host: host, port: port };
 
 		debug.proxyRequest('connecting to proxy target %j', opts);
+		debug.proxyRequest('method %j', req.method);
+
+		let statusCode = 200;
+		const chaosPercentage = 70;
+		const chance = Math.floor(Math.random() * 100);
+		debug.proxyRequest('chance %j < %j', chance, chaosPercentage);
+		if (chance < chaosPercentage) {
+			statusCode = getRandomStatusCode('GET');
+		}
+		debug.proxyRequest('status code %j', statusCode);
+
+		if (statusCode > 200) {
+			if (statusCode === 429) {
+				res.setHeader('retry-after', '2');
+			}
+			res.writeHead(statusCode);
+			res.end(JSON.stringify({
+				error: {
+					code: httpStatusCode[statusCode],
+					message: 'Some error Happened'
+				}
+			}));
+			return;
+		}
+
 		target = net.connect(opts);
 		target.on('connect', ontargetconnect);
 		target.on('close', ontargetclose);
@@ -479,4 +504,83 @@ function requestAuthorization(req, res) {
 	};
 	res.writeHead(407, headers);
 	res.end();
+}
+
+// CHAOS
+
+const methodStatusCode = {
+	GET: [429, 500, 502, 503, 504],
+	POST: [429, 500, 502, 503, 504, 507],
+	PUT: [429, 500, 502, 503, 504, 507],
+	PATCH: [429, 500, 502, 503, 504],
+	DELETE: [429, 500, 502, 503, 504, 507]
+};
+
+const httpStatusCode = {
+	100: "Continue",
+	101: "Switching Protocols",
+	102: "Processing",
+	103: "Early Hints",
+	200: "OK",
+	201: "Created",
+	202: "Accepted",
+	203: "Non-Authoritative Information",
+	204: "No Content",
+	205: "Reset Content",
+	206: "Partial Content",
+	207: "Multi-Status",
+	208: "Already Reported",
+	226: "IM Used",
+	300: "Multiple Choices",
+	301: "Moved Permanently",
+	302: "Found",
+	303: "See Other",
+	304: "Not Modified",
+	305: "Use Proxy",
+	307: "Temporary Redirect",
+	308: "Permanent Redirect",
+	400: "Bad Request",
+	401: "Unauthorized",
+	402: "Payment Required",
+	403: "Forbidden",
+	404: "Not Found",
+	405: "Method Not Allowed",
+	406: "Not Acceptable",
+	407: "Proxy Authentication Required",
+	408: "Request Timeout",
+	409: "Conflict",
+	410: "Gone",
+	411: "Length Required",
+	412: "Precondition Failed",
+	413: "Payload Too Large",
+	414: "URI Too Long",
+	415: "Unsupported Media Type",
+	416: "Range Not Satisfiable",
+	417: "Expectation Failed",
+	421: "Misdirected Request",
+	422: "Unprocessable Entity",
+	423: "Locked",
+	424: "Failed Dependency",
+	425: "Too Early",
+	426: "Upgrade Required",
+	428: "Precondition Required",
+	429: "Too Many Requests",
+	431: "Request Header Fields Too Large",
+	451: "Unavailable For Legal Reasons",
+	500: "Internal Server Error",
+	501: "Not Implemented",
+	502: "Bad Gateway",
+	503: "Service Unavailable",
+	504: "Gateway Timeout",
+	505: "HTTP Version Not Supported",
+	506: "Variant Also Negotiates",
+	507: "Insufficient Storage",
+	508: "Loop Detected",
+	510: "Not Extended",
+	511: "Network Authentication Required",
+};
+
+function getRandomStatusCode(requestMethod) {
+	const statusCodeArray = methodStatusCode[requestMethod];
+	return statusCodeArray[Math.floor(Math.random() * statusCodeArray.length)];
 }
